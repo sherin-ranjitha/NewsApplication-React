9dac1a68acd60f954d67e6f4912ebb18d92cce7e
sherin-ranjitha <132578505+sherin-ranjitha@users.noreply.github.com>
2023-05-11 11:52:15 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NewsItem = (props) =>{
+  
+    let {title, description, imageUrl, newsUrl,author,date,source}= props;
+    return (
+      <div className="my-3">
+        <div className="card" >
+          <div style={{display: 'flex',
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        right: '0'}}>
+        <span className="badge rounded-pill bg-danger">
+          {source}
+          </span>
+          </div>
+          <img src= {!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPgK4IF1erwUfmYefSmPAXSkEwGwCrORV20A&usqp=CAU": imageUrl} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default NewsItem;
